8142bef799bac4f0349a331386795245c65041b5
FolexDev <alex@mediaonegroup.ch>
2026-05-12 10:59:55 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE = 'mog-send-v1';
+const ASSETS = ['./','./manifest.json','./icon-192.png','./icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
